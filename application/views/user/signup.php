d174e28b18e4130cc497143d5921e678acb7e0ef
haidarvm <haidarvm@gmail.com>
2016-04-02 12:04:57 +0700

    remove lomba

--- application/views/user/signup.php
+++ application/views/user/signup.php
@@ -77,11 +77,6 @@
 								<label>Kota</label>
 								<input type="text" name="city" class="form-control">
 							</div>
-							<div class="form-group">
-								<label>Ikuti Lomba foto
-								<input type="checkbox" name="lomba_foto" value="1">
-								</label>
-							</div>
 							
 							<button type="submit" class="btn btn-success">Sign up</button>
 							
