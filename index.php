6424ed69b711d725552b46603ea6790d75894b83
haidarvm <haidarvm@gmail.com>
2016-04-02 15:54:38 +0700

    remove vendor

--- index.php
+++ index.php
@@ -289,6 +289,6 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
-require FCPATH . 'vendor/autoload.php';
-require FCPATH . 'application/config/autoload.php';
+#require FCPATH . 'vendor/autoload.php';
+#require FCPATH . 'application/config/autoload.php';
 require_once BASEPATH.'core/CodeIgniter.php';
