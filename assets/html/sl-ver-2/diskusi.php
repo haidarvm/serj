62e408201dcfd17d5bd4478e2831095cf8235ebc
alfonkdp <alfon.krisna@paraplou.com>
2016-04-24 19:09:07 +0700

    add pagination di halaman diskusi + add hasil pencarian diskusi

--- assets/html/sl-ver-2/diskusi.php
+++ assets/html/sl-ver-2/diskusi.php
@@ -747,6 +747,29 @@
 							</div>
 							
 							
+							<!-- pagination -->
+							<div class="load-more col-md-12">
+								<nav>
+								  <ul class="pagination">
+									<li class="disabled">
+									  <a href="#" aria-label="Previous">
+										<span aria-hidden="true">&laquo; Sebelumnya</span>
+									  </a>
+									</li>
+									<li class="active"><a href="#">1</a></li>
+									<li><a href="#">2</a></li>
+									<li><a href="#">3</a></li>
+									<li><a href="#">4</a></li>
+									<li><a href="#">5</a></li>
+									<li>
+									  <a href="#" aria-label="Next">
+										<span aria-hidden="true">Berikutnya &raquo;</span>
+									  </a>
+									</li>
+								  </ul>
+								</nav>
+							</div>
+							
 						</div>
 					</div>
 				</div>
