5c8a4bc8aafa1500d2678516e722804abfe44aff
haidar <haidarvm@gmail.com>
2016-07-18 21:40:23 +0700

    kodefikasi

---
+++ kemenpan/kodefikasi.php
@@ -0,0 +1,21 @@
+<?php include('header.php')?>
+<div class="content">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="card">
+					<div class="header">
+						<h4 class="title">
+							<b>KODEFIKASI</b>
+						</h4>
+						<!-- <p class="category">Last Campaign Performance</p> -->
+					</div>
+					<div class="content">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include('footer.php')?>
