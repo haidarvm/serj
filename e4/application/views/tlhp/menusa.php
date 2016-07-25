b57f816733ab13cfeacd21b0df501d4cad6fd57e
siregaraldo <siregaraldo@gmail.com>
2016-07-25 22:46:51 +0700

    design manusa

--- e4/application/views/tlhp/menusa.php
+++ e4/application/views/tlhp/menusa.php
@@ -1,13 +1,222 @@
 <div class="content">
-	<div class="row">
+    <div class="row">
+            <h4 class="tittle-row">TINDAK LANJUT HASIL PENGAWASAN</h4>
+            <div class="col-lg-2 col-sm-6">
+                <div class="card">
+	                   <div class="content">
+	                       <div class="row">
+	                           <div class="col-xs-12">
+	                               <div class="numbers">
+	                                   <p class="t-card">TOTAL TEMUAN SEJAK TAHUN 2013</p>
+                                       <span class="t-number">50</span>
+	                               </div>
+	                           </div>
+	                       </div>
+	                   </div>
+				       <div class="card-footer">
+				            <hr>
+						  <div class="stats">
+							<i class="ti-timer"></i> Update 2 days ago
+						  </div>
+				       </div>
+	               </div>
+	           </div>
+	           <div class="col-lg-2 col-sm-6">
+                <div class="card">
+	                   <div class="content">
+	                       <div class="row">
+	                           <div class="col-xs-12">
+	                               <div class="numbers">
+	                                   <p class="t-card">TOTAL REKOMENDASI SEJAK TAHUN 2013</p>
+                                       <span class="t-number">70</span>
+	                               </div>
+	                           </div>
+	                       </div>
+	                   </div>
+				       <div class="card-footer">
+				            <hr>
+						  <div class="stats">
+							<i class="ti-timer"></i> Update 2 days ago
+						  </div>
+				       </div>
+	               </div>
+	           </div>
+              <div class="col-lg-2 col-sm-6">
+                <div class="card">
+	                   <div class="content">
+	                       <div class="row">
+	                           <div class="col-xs-12">
+	                               <div class="numbers">
+	                                   <p class="t-card">BELUM SESUAI REKOMENDASI</p>
+                                       <span class="t-number">50</span>
+	                               </div>
+	                           </div>
+	                       </div>
+	                   </div>
+				       <div class="card-footer">
+				            <hr>
+						  <div class="stats">
+							<i class="ti-timer"></i> Update 2 days ago
+						  </div>
+				       </div>
+	               </div>
+	           </div>
+	           <div class="col-lg-2 col-sm-6">
+                <div class="card">
+	                   <div class="content">
+	                       <div class="row">
+	                           <div class="col-xs-12">
+	                               <div class="numbers">
+	                                   <p class="t-card">BELUM DITINDAKLANJUTI</p>
+                                       <span class="t-number">50</span>
+	                               </div>
+	                           </div>
+	                       </div>
+	                   </div>
+				       <div class="card-footer">
+				            <hr>
+						  <div class="stats">
+							<i class="ti-timer"></i> Update 2 days ago
+						  </div>
+				       </div>
+	               </div>
+	           </div>
+	           <div class="col-lg-2 col-sm-6">
+                <div class="card">
+	                   <div class="content">
+	                       <div class="row">
+	                           <div class="col-xs-12">
+	                               <div class="numbers">
+	                                   <p class="t-card">TIDAK DAPAT DITINDAKLANJUTI</p>
+                                       <span class="t-number">30</span>
+	                               </div>
+	                           </div>
+	                       </div>
+	                   </div>
+				       <div class="card-footer">
+                           <hr>
+						  <div class="stats">
+							<i class="ti-timer"></i> Update 2 days ago
+						  </div>
+				       </div>
+	               </div>
+	           </div>
+	           <div class="col-lg-2 col-sm-6">
+                <div class="card">
+	                   <div class="content">
+	                       <div class="row">
+	                           <div class="col-xs-12">
+	                               <div class="numbers">
+	                                   <p class="t-card">TOTAL TEMUAN SEJAK TAHUN 2013</p>
+                                       <span class="t-number">10</span>
+	                               </div>
+	                           </div>
+	                       </div>
+	                   </div>
+				       <div class="card-footer">
+				            <hr>
+						  <div class="stats">
+							<i class="ti-timer"></i> Update 2 days ago
+						  </div>
+				       </div>
+	               </div>
+	           </div>
+	   </div>
+    
+        <div class="row">
+            <h4 class="tittle-row">KERUGIAN KEUANGAN NEGARA</h4>
+            <div class="col-lg-3 col-sm-6">
+                <div class="card">
+	                   <div class="content">
+	                       <div class="row">
+	                           <div class="col-xs-12">
+	                               <div class="numbers">
+	                                   <p class="t-card">KERUGIAN NEGARA SEJAK TAHUN 2013</p>
+                                       <span class="t-number">RP 8.000.000</span>
+	                               </div>
+	                           </div>
+	                       </div>
+	                   </div>
+				       <div class="card-footer">
+				            <hr>
+						  <div class="stats">
+							<i class="ti-timer"></i> Update 2 days ago
+						  </div>
+				       </div>
+	               </div>
+	           </div>
+	           <div class="col-lg-3 col-sm-6">
+                <div class="card">
+	                   <div class="content">
+	                       <div class="row">
+	                           <div class="col-xs-12">
+	                               <div class="numbers">
+	                                   <p class="t-card">TELAH DISTOR KE KAS NEGARA</p>
+                                       <span class="t-number">RP 5.000.000</span>
+	                               </div>
+	                           </div>
+	                       </div>
+	                   </div>
+				       <div class="card-footer">
+				            <hr>
+						  <div class="stats">
+							<i class="ti-timer"></i> Update 2 days ago
+						  </div>
+				       </div>
+	               </div>
+	           </div>
+               <div class="col-lg-3 col-sm-6">
+                <div class="card">
+	                   <div class="content">
+	                       <div class="row">
+	                           <div class="col-xs-12">
+	                               <div class="numbers">
+	                                   <p class="t-card">BELUM DISTOR KE KAS NEGARA</p>
+                                       <span class="t-number">RP 3.000.000</span>
+	                               </div>
+	                           </div>
+	                       </div>
+	                   </div>
+                    <div class="card-footer">
+				            <hr>
+						  <div class="stats">
+							<i class="ti-timer"></i> Update 2 days ago
+						  </div>
+				       </div>
+	               </div>
+	           </div>
+            <div class="col-lg-3 col-sm-6">
+                
+	                   <div class="card" data-color="magenta">
+	                       <div class="content">
+	                           <div class="row">
+	                               <div class="col-xs-3">
+	                                   <div class="icon-big icon-success text-center">
+	                                   <i class="fa fa-print fa-lg"></i>
+	                                   </div>
+	                               </div>
+	                                   <div class="col-xs-9">
+	                                   <div class="numbers">
+	                                   <p>CETAK DOKUMEN</p>
+                                          <button class="btn btn-danger btn-fill btn-wd"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> PDF</button>
+                                          <button class="btn btn-info btn-fill btn-wd"><i class="fa fa-file-word-o" aria-hidden="true"></i> MS WORD</button>
+	                                   </div>
+	                                   </div>
+	                                </div>
+	                            </div>
+	                   </div>
+                    
+	       </div>
+    </div>
+ <!--div class="row">
 		<div class="col-md-6">
 			<div class="card">
 				<div class="header">
 					<h4 class="title">
 						<b>TINDAK LANJUT HASIL PENGAWASAN</b>
 					</h4>
-					<!--                                 <p class="category">Last Campaign Performance</p> -->
-				</div>
+					<!-- <p class="category">Last Campaign Performance</p> -->
+				<!--/div>
 				<div class="content">
 					<div class="inner col-md-10">
 						<p class="h6">TOTAL TEMUAN SEJAK TAHUN 2013</p>
@@ -28,15 +237,15 @@
 				</div>
 				<div class="clearfix"></div>
 			</div>
-		</div>
-		<div class="col-md-6">
+		</div-->
+		<!--div class="col-md-6">
 			<div class="card ">
 				<div class="header">
 					<h4 class="title">
 						<b>KERUGIAN KEUANGAN NEGARA</b>
 					</h4>
 					<!--                                 <p class="category">All products including Taxes</p> -->
-				</div>
+				<!--/div>
 				<div class="content">
 					<div class="inner col-md-8">
 						<p class="h6">KERUGIAN NEGARA SEJAK TAHUN 2013</p>
@@ -60,10 +269,11 @@
 				<div class="clearfix"></div>
 			</div>
 		</div>
-	</div>
+	</div-->
 
 
 	<div class="row">
+        <h4 class="tittle-row">PIE CHART</h4>
 		<div class="col-md-6">
 			<div class="card">
 				<div class="header">
@@ -77,12 +287,12 @@
 						height="200" style="width: 300px; height: 200px;"></canvas>
 
 					<div class="footer">
-						<div class="chart-legend">
+						<!--div class="chart-legend">
 							<i class="fa fa-circle text-info"></i> Tidak dapat tindak lanjut
 							<i class="fa fa-circle text-danger"></i> Selesai tindak lanjut <i
 								class="fa fa-circle text-warning"></i> Belum sesuai <i
 								class="fa fa-circle text-success"></i> Belum tindak lanjut
-						</div>
+						</div-->
 						<hr>
 						<div class="stats">
 							<i class="ti-timer"></i> Update 2 days ago
@@ -104,12 +314,12 @@
 						height="200" style="width: 300px; height: 200px;"></canvas>
 
 					<div class="footer">
-						<div class="chart-legend">
+						<!--div class="chart-legend">
 							<i class="fa fa-circle text-info"></i> Tidak dapat tindak lanjut
 							<i class="fa fa-circle text-danger"></i> Selesai tindak lanjut <i
 								class="fa fa-circle text-warning"></i> Belum selesai <i
 								class="fa fa-circle text-success"></i> Belum tindak lanjut
-						</div>
+						</div-->
 						<hr>
 						<div class="stats">
 							<i class="ti-timer"></i> Update 2 days ago
