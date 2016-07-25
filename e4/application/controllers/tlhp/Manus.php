98224223d9db70342d0cb74eb174d06ef59f74aa
haidar <haidarvm@gmail.com>
2016-07-25 22:40:50 +0700

    add litle css

--- e4/application/controllers/tlhp/Manus.php
+++ e4/application/controllers/tlhp/Manus.php
@@ -20,24 +20,23 @@ class Manus extends MY_Controller {
 	 */
 	public function index() {
 		$data['title'] = "Management User";
-		//$data['getAll'] = $this->muser->getAllUser();
+		// $data['getAll'] = $this->muser->getAllUser();
 		$this->load->tlhp_template('tlhp/manus', $data);
 	}
-	
+
 	public function stat() {
 		$data['title'] = "Stat";
-		//$data['getAll'] = $this->muser->getAllUser();
+		// $data['getAll'] = $this->muser->getAllUser();
 		$this->load->tlhp_template('tlhp/stat', $data);
-		
 	}
-	
+
 	/**
 	 * For All User
 	 */
 	function get_all_user() {
 		$requestData = $this->input->post();
 		// print_r($requestData['search']['value']);exit;
-		$columns = array( 0 => 'user_id', 1 => 'full_name', 2 => 'nip', 3 => 'jabatan_id', 4 => 'username', 5 => 'unit_kerja', 6 => 'user_level' );
+		$columns = array(0 => 'user_id', 1 => 'full_name', 2 => 'nip', 3 => 'jabatan', 4 => 'username', 5 => 'unit_kerja', 6 => 'user_level');
 		// echo $sql;
 		// $order_by = " ORDER BY employee_name LIMIT 10,10 ";
 		$query = $this->muser->getAllUserDt($cond = NULL, $order_by = NULL);
@@ -48,7 +47,7 @@ class Manus extends MY_Controller {
 		if (! empty($requestData['search']['value'])) {
 			$cond = "AND ( full_name LIKE '%" . $requestData['search']['value'] . "%' ";
 			$cond .= "OR unit_kerja LIKE '%" . $requestData['search']['value'] . "%' ";
-			$cond .= " OR user_level LIKE '%" . $requestData['search']['value'] . "%' )";
+			$cond .= " OR jabatan LIKE '%" . $requestData['search']['value'] . "%' )";
 			// echo 'masuk';
 		} else {
 			$cond = NULL;
@@ -59,25 +58,36 @@ class Manus extends MY_Controller {
 		$query3 = $this->muser->getAllUserDt($cond, $order_by3);
 		;
 		$data = array();
-		foreach ($query3->result_array() as $row ) {
+		foreach ( $query3->result_array() as $row ) {
 			$nestedData = array();
 			$nestedData[] = $row["user_id"];
 			$nestedData[] = $row["full_name"];
 			$nestedData[] = $row["nip"];
-			$nestedData[] = $row["jabatan_id"];
+			$nestedData[] = $row["jabatan"];
 			$nestedData[] = $row["username"];
 			$nestedData[] = $row["unit_kerja"];
 			$nestedData[] = $row["user_level"];
 			$data[] = $nestedData;
 		}
-		$json_data = array( "draw" => intval($requestData['draw']), "recordsTotal" => intval($totalData), "recordsFiltered" => intval($totalFiltered), "data" => $data );
+		$json_data = array("draw" => intval($requestData['draw']), "recordsTotal" => intval($totalData), "recordsFiltered" => intval($totalFiltered), "data" => $data);
 		echo json_encode($json_data);
 	}
 
-	/**
-	 * List All Product
-	 *
-	 * @param string Category slug
-	 */
-	
+	function update_user($id = NULL) {
+		$data['title'] = "username";
+		$data['user'] = '';
+		$this->load->view('tlhp/user', $data);
+	}
+
+	function add_user() {
+		$data['title'] = "username";
+		$this->load->view('tlhp/user', $data);
+	}
+
+/**
+ * List All Product
+ *
+ * @param
+ *        	string Category slug
+ */
 }
--- e4/assets/js/tlhp.js
+++ e4/assets/js/tlhp.js
@@ -1,32 +1,30 @@
 $(document).ready(function() {
-// Management User 
- 	 $(document).ready(function() {
-                var dataTable = $('#manus-grid').DataTable( {
-                    "processing": true,
-                    "serverSide": true,
-                    "order": [[ 0, "desc" ]],
-                    "ajax":{
-                        url : site_url + 'tlhp/manus/get_all_user', // json data
-                        type: "post",  
-                        error: function(){  // error handling
-                            $(".data-grid-error").html("");
-                            $("#data-grid").append('<tbody class="data-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
-                            $("#data-grid_processing").css("display","none");
-                        }
-                    },
+	// Management User 
+	var dataTable = $('#manus-grid').DataTable( {
+            "processing": true,
+            "serverSide": true,
+            "order": [[ 0, "desc" ]],
+            "ajax":{
+                url : site_url + 'tlhp/manus/get_all_user', // json data
+                type: "post",  
+                error: function(){  // error handling
+                    $(".data-grid-error").html("");
+                    $("#data-grid").append('<tbody class="data-grid-error"><tr><th colspan="3">No data found in the server</th></tr></tbody>');
+                    $("#data-grid_processing").css("display","none");
+                }
+            },
 
-					"fnRowCallback": function( nRow, aData, iDisplayIndex ) {
-					    $('td:eq(1)', nRow).html('<a href="'+ site_url + 'tlhp/manus/detail/' + aData[0] + '">' + aData[1] + '</a>');
-					    return nRow;
-					  },
+			"fnRowCallback": function( nRow, aData, iDisplayIndex ) {
+			    $('td:eq(1)', nRow).html('<a href="'+ site_url + 'tlhp/manus/detail/' + aData[0] + '">' + aData[1] + '</a>');
+			    return nRow;
+			  },
 
-					"aoColumnDefs": [{
-						"aTargets": [7],
-						mRender: function ( data, type, row ) {
-							return '<div class="btn-group"><a href="'+ site_url + 'tlhp/manus/detail/'+ row[0]+'" class="btn btn-primary btn-xs"><i class="fa fa-eye"></i> Detail</a> &nbsp; <a href="#" data-toggle="modal" data-target="#confirm-delete-modal" data-href="'+ site_url + 'tlhp/manus/delete/'+ row[0]+'" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> Delete</a></div>';
-						}
-					}]
-          });
+			"aoColumnDefs": [{
+				"aTargets": [7],
+				mRender: function ( data, type, row ) {
+					return '<div class="btn-group"><a href="'+ site_url + 'tlhp/manus/detail/'+ row[0]+'" class="btn btn-primary btn-xs"><i class="fa fa-eye"></i> Detail</a> &nbsp; <a href="#" data-toggle="modal" data-target="#confirm-delete-modal" data-href="'+ site_url + 'tlhp/manus/delete/'+ row[0]+'" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> Delete</a></div>';
+				}
+			}]
       });
  	 
  	// add tim
@@ -45,5 +43,11 @@ $(document).ready(function() {
 		var add_more = $('.add-more-tim-perpanjangan').last().clone(true).show();
 //			$('#noper-more').append($('.add-more-noper').show());
 		$('#tim-more').append(add_more);
+	});
+	
+	// load user
+	$('.user-modal').on('click', function(e){
+		  e.preventDefault();
+		  $('#userModal').modal('show').find('.modal-body').load($(this).attr('href'));
 	});
 });
