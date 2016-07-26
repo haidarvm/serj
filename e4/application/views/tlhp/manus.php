d7800efc1eb02576211aecd0333a0e14efd01466
haidar <haidarvm@gmail.com>
2016-07-26 20:37:10 +0700

    manus dialog

--- e4/application/views/tlhp/manus.php
+++ e4/application/views/tlhp/manus.php
@@ -35,12 +35,12 @@
 										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 											<span aria-hidden="true">&times;</span>
 										</button>
-										<h4 class="modal-title" id="userModalLabel">Edit User</h4>
+										<h4 class="modal-title" id="userModalLabel">User</h4>
 									</div>
 									<div class="modal-body modal-form"></div>
 									<div class="modal-footer">
-										<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-										<button type="button" class="btn btn-primary">Send message</button>
+										<button type="button" class="btn btn-default" data-dismiss="modal">BATAL</button>
+										<button type="button" class="btn btn-primary">SIMPAN</button>
 									</div>
 								</div>
 							</div>
