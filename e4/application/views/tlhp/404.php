55d3adc9c2abeb2d697265b9a8f4a1f71b3afeae
siregaraldo <siregaraldo@gmail.com>
2016-07-28 19:57:43 +0700

    underconstruction page

--- e4/application/views/tlhp/404.php
+++ e4/application/views/tlhp/404.php
@@ -1,43 +1,20 @@
 <div class="content">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-lg-12 col-md-7">
-				<div class="card">
-					<div class="content">
-						<div class="header">
-							<h3 class="title must-center">PILIH LHP</h3>
-						</div>
-						<div class="row">
-							<div class="col-md-7 col-md-offset-2 top-space">
-								<form class="form-horizontal" action="<?=site_url()?>tlhp/pilihlhp/updatelhp" method="POST">
-									<div class="form-group">
-										<label for="inputEmail3" class="col-sm-3 control-label t-left">TAHUN</label>
-										<div class="col-sm-9">
-											<select class="form-control" name="tanngal_lhp">
-												<option>2016</option>
-											</select>
-										</div>
-									</div>
-									<div class="form-group">
-										<label for="inputEmail3" class="col-sm-3 control-label t-left">JUDUL LHP</label>
-										<div class="col-sm-9">
-											<select class="form-control" name="lhp_id">
-													<?php if ($getAllLHP) {?>
-														<?php foreach($getAllLHP as $lhp) {?>
-														<option value="<?=$lhp->lhp_id;?>"><?=$lhp->judul_lhp;?></option>
-														<?php } ?>
-													<?php } ?>
-											</select>
-										</div>
-									</div>
-									<div class="col-md-12 top-space must-center">
-										<button type="submit" class="btn btn-info btn-fill btn-wd bottom-space">UPDATE LHP</button>
-									</div>
-								</form>
-							</div>
-						</div>
+			<div class="col-md-12">
+                <div class="col-xs-6 col-md-4 col-md-offset-5">
+                        
+                </div>
+					<div class="col-xs-5 col-md-8 under-con col-md-offset-6">
+						
+                        <div class="logo-under col-md-offset-2">
+					<a href="<?php echo site_url();?>dashboard/"><img src="<?php echo base_url();?>assets/img/traffic-cone.png"></a>
+                            <h3 class="under-text">UNDER CONSTRUCTION</h3>
+                            <p class="h6">This page is coming soon <a href="<?php echo site_url();?>dashboard/">back to home</a></p>
+				        </div>
 					</div>
-				</div>
+               
+
 			</div>
 		</div>
 	</div>
