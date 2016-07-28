0372480f79f3e5e332fc9c9eb242c30ac82b111e
haidar <haidarvm@gmail.com>
2016-07-28 19:16:11 +0700

    alter table user

--- e4/assets/db/alter.sql
+++ e4/assets/db/alter.sql
@@ -1,1 +1,2 @@
 UPDATE `tlhp_user` SET `username` = 'admin', `password` = MD5('admin') WHERE `tlhp_user`.`user_id` = 5;
+ALTER TABLE `tlhp_user` CHANGE `jabatan_id` `jabatan` VARCHAR(255) NULL DEFAULT NULL;
