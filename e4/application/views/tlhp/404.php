c1f947a242f08b5e027414a68b8348270e44a14c
alfon_ <alfon@fabelio.com>
2016-07-28 21:58:04 +0700

    updated 404 & cons

--- e4/application/views/tlhp/404.php
+++ e4/application/views/tlhp/404.php
@@ -8,7 +8,7 @@
 					<div class="col-xs-5 col-md-8 under-con col-md-offset-4">
 						
                         <div class="logo-under col-md-offset-2">
-					<img src="<?php echo base_url();?>assets/img/browser.png">
+							<img src="<?php echo base_url();?>assets/img/browser.png">
                             <h3 class="error-text">ERROR 404: PAGE NOT FOUND</h3>
                             <p class="h6">Uh Oh! Please try one of the following pages <a href="<?php echo site_url();?>dashboard/">back to home</a></p>
 				        </div>
--- e4/application/views/tlhp/underconstruction.php
+++ e4/application/views/tlhp/underconstruction.php
@@ -5,7 +5,7 @@
                 <div class="col-xs-6 col-md-4 col-md-offset-5">
                         
                 </div>
-					<div class="col-xs-5 col-md-8 under-con col-md-offset-6">
+					<div class="col-xs-5 col-md-8 under-con col-md-offset-4">
 						
                         <div class="logo-under col-md-offset-2">
 					           <img src="<?php echo base_url();?>assets/img/traffic-cone.png">
