8d0696ee86fae7438018649be105a2f4e460f869
siregaraldo <siregaraldo@gmail.com>
2016-07-28 17:28:08 +0700

    merge

--- e4/application/views/tlhp/underconstruction.php
+++ e4/application/views/tlhp/underconstruction.php
@@ -1,43 +1,20 @@
 <div class="content">
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-lg-12 col-md-7">
-				<div class="card">
-					<div class="content">
-						<div class="header">
-							<h3 class="title must-center">PILIH LHP</h3>
-						</div>
-						<div class="row">
-							<div class="col-md-7 col-md-offset-2 top-space">
-								<form class="form-horizontal" action="<?=site_url()?>tlhp/pilihlhp/updatelhp" method="POST">
-									<div class="form-group">
-										<label for="inputEmail3" class="col-sm-3 control-label t-left">TAHUN</label>
-										<div class="col-sm-9">
-											<select class="form-control" name="tanngal_lhp">
-												<option>2016</option>
-											</select>
-										</div>
-									</div>
-									<div class="form-group">
-										<label for="inputEmail3" class="col-sm-3 control-label t-left">JUDUL LHP</label>
-										<div class="col-sm-9">
-											<select class="form-control" name="lhp_id">
-													<?php if ($getAllLHP) {?>
-														<?php foreach($getAllLHP as $lhp) {?>
-														<option value="<?=$lhp->lhp_id;?>"><?=$lhp->judul_lhp;?></option>
-														<?php } ?>
-													<?php } ?>
-											</select>
-										</div>
-									</div>
-									<div class="col-md-12 top-space must-center">
-										<button type="submit" class="btn btn-info btn-fill btn-wd bottom-space">UPDATE LHP</button>
-									</div>
-								</form>
-							</div>
-						</div>
+			<div class="col-md-12">
+                <div class="col-xs-6 col-md-4">
+                </div>
+			
+					<div class="col-xs-6 col-md-4">
+                   
+						<h3 class="under-text">UNDER CONSTRUCTION</h3>
+                        <p>This page is coming soon</p>
+                             <div class="logo-under">
+					<a href="<?php echo site_url();?>dashboard/"><img src="<?php echo base_url();?>assets/img/logo-black.png"></a>
+				        </div>
 					</div>
-				</div>
+                <div class="col-xs-6 col-md-4"></div>
+
 			</div>
 		</div>
 	</div>
