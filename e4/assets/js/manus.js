95d6edbd292aaa62533d9f45aa13d4f94fd3045e
Atang Sutisna <atang@bippo.co.id>
2016-07-31 07:36:56 +0700

    bug fixing edit user

--- e4/assets/js/manus.js
+++ e4/assets/js/manus.js
@@ -1,6 +1,11 @@
 define(["jquery", "bootstrap", "data-table"], function($){
+	
+	window.userEdit = function userEdit(id) {
+    	var body = site_url + 'tlhp/manus/update/'+ id;
+    	$('#userModal').modal('show').find('.modal-body').load(body);
+    }
+	
     $(function(){
-        
         $('#manus-grid').DataTable( {
             "processing": true,
             "serverSide": true,
@@ -15,14 +20,14 @@ define(["jquery", "bootstrap", "data-table"], function($){
                 }
             },
             "fnRowCallback": function( nRow, aData, iDisplayIndex ) {
-                $('td:eq(1)', nRow).html('<a onclick="userEdit(' + aData[0] + ')" href="javascript:;">' + aData[1] + '</a>');
+                $('td:eq(1)', nRow).html('<a onclick="userEdit(' + aData[0] + ')" href="javascript:;" class="fire">' + aData[1] + '</a>');
                 return nRow;
             },
             "aoColumnDefs": [{
                     "aTargets": [7],
                     mRender: function ( data, type, row ) {
                             //return '<div class="btn-group"><a onclick="userEdit()"  href="'+ site_url + 'tlhp/manus/update_user/'+ row[0]+'" class="user-modal-edit  btn btn-primary btn-xs"><i class="fa fa-eye"></i> Edit</a> &nbsp; <a href="#" data-toggle="modal" data-target="#confirm-delete-modal" data-href="'+ site_url + 'tlhp/manus/delete/'+ row[0]+'" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> Delete</a></div>';
-                            return '<div class="btn-group"><a onclick="userEdit('+row[0]+')" href="javascript:;" class="user-modal-edit  btn btn-primary btn-xs"><i class="fa fa-eye"></i> Edit</a> &nbsp; <a onclick="return confirm('+"'Anda yakin ingin menghapus data ini ...?'"+')"  href="'+ site_url + 'tlhp/manus/delete/'+ row[0]+'" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> Delete</a></div>';
+                            return '<div class="btn-group"><a onClick="userEdit('+row[0]+')" href="javascript:;" class="user-modal-edit  btn btn-primary btn-xs"><i class="fa fa-eye"></i> Edit</a> &nbsp; <a onclick="return confirm('+"'Anda yakin ingin menghapus data ini ...?'"+')"  href="'+ site_url + 'tlhp/manus/delete/'+ row[0]+'" class="btn btn-danger btn-xs"><i class="fa fa-trash"></i> Delete</a></div>';
 
                     }
             }]
@@ -33,4 +38,5 @@ define(["jquery", "bootstrap", "data-table"], function($){
         $('#userModal').modal('show');
         return false;
     });
+    
  });
