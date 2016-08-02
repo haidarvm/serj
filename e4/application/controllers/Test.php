caa7cf39d97c2f921581b60ffd987d1bf2769e1f
alfon_ <alfon@fabelio.com>
2016-08-02 10:54:23 +0700

    table menusa

--- e4/application/controllers/Test.php
+++ e4/application/controllers/Test.php
@@ -53,34 +53,45 @@ class Test extends MY_Controller {
 		// $data['headers'] = array('TINDAK LANJUT HASIL PENGAWASAN', 'KERUGIAN KEUANGAN NEGARA');
 		// $reportMenusa = $this->muser->getAllLogUser($cond = NULL, $order_by = ' ORDER BY log_id DESC')->result();
 		$reportMenusa = "<tr>
-							<td colspan='2'>TINDAK LANJUT HASIL PENGAWASAN</td>
-							<td colspan='2'>KERUGIAN KEUANGAN NEGARA</td>
+							<th colspan='2'>TINDAK LANJUT HASIL PENGAWASAN</th>
+							<th colspan='2'>KERUGIAN KEUANGAN NEGARA</th>
 						</tr>
 						<tr>
-							<td>TOTAL TEMUAN SEJAK TAHUN 2013	:</td>
-							<td>50</td>
+							<td>TOTAL TEMUAN SEJAK TAHUN 2013</td>
+							<td>: 50</td>
+							<td>TOTAL TEMUAN SEJAK TAHUN 2013</td>
+							<td>: 50</td>
 						</tr>	
 						<tr>
-							<td>TOTAL REKOMENDASI SEJAK TAHUN 2013	:</td>
-							<td>70</td>
+							<td>TOTAL REKOMENDASI SEJAK TAHUN 2013</td>
+							<td>: 70</td>
+							<td>TOTAL REKOMENDASI SEJAK TAHUN 2013</td>
+							<td>: 70</td>
 						</tr>
 						<tr>
-							<td>SELESAI DITINDAKLANJUTI		:</td>
-							<td>50</td>
+							<td>SELESAI DITINDAKLANJUTI</td>
+							<td>: 50</td>
+							<td>SELESAI DITINDAKLANJUTI</td>
+							<td>: 50</td>
 						</tr>
 						<tr>
-							<td>BELUM SESUAI REKOMENDASI		:</td>
-							<td>15</td>
+							<td>BELUM SESUAI REKOMENDASI</td>
+							<td>: 15</td>
+							<td>BELUM SESUAI REKOMENDASI</td>
+							<td>: 15</td>
 						</tr>
 						<tr>
-							<td>BELUM DITINDAKLANJUTI		:</td>
-							<td>30</td>
+							<td>BELUM DITINDAKLANJUTI</td>
+							<td>: 30</td>
+							<td>BELUM DITINDAKLANJUTI</td>
+							<td>: 30</td>
 						</tr>
 						<tr>
-							<td>TIDAK DAPAT DITINDAKLANJUTI		:</td>
-							<td>2</td>
+							<td>TIDAK DAPAT DITINDAKLANJUTI</td>
+							<td>: 2</td>
+							<td>TIDAK DAPAT DITINDAKLANJUTI</td>
+							<td>: 2</td>
 						</tr>
-				
 						";
 		$data['report'] = $reportMenusa;
 		$this->pdf($data, "menusa");
