75286abbe799878351fdb852fd689e2ba5dadbac
haidar <haidarvm@gmail.com>
2016-08-02 16:23:36 +0700

    db alter combine

--- e4/assets/db/e4_new.sql
+++ e4/assets/db/e4_new.sql
@@ -2553,3 +2553,13 @@ LOCK TABLES `tlhp_orix_user_level` WRITE;
 INSERT INTO `tlhp_orix_user_level` VALUES (1,'super admin'),(2,'admin'),(3,'user');
 /*!40000 ALTER TABLE `tlhp_orix_user_level` ENABLE KEYS */;
 UNLOCK TABLES;
+
+
+UPDATE `tlhp_bpk_user` SET `active` = '1' ;
+UPDATE `tlhp_bpkp_user` SET `active` = '1' ;
+UPDATE `tlhp_ikp_user` SET `active` = '1' ;
+UPDATE `tlhp_orix_user` SET `active` = '1' ;
+ALTER TABLE `tlhp_bpk_user` CHANGE `active` `active` TINYINT(1) NOT NULL DEFAULT '1';
+ALTER TABLE `tlhp_bpkp_user` CHANGE `active` `active` TINYINT(1) NOT NULL DEFAULT '1';
+ALTER TABLE `tlhp_ikp_user` CHANGE `active` `active` TINYINT(1) NOT NULL DEFAULT '1';
+ALTER TABLE `tlhp_orix_user` CHANGE `active` `active` TINYINT(1) NOT NULL DEFAULT '1';
