cc24cc58f4ca4eac3db6d44324406c7013539442
alfon_ <alfon@fabelio.com>
2016-08-02 11:28:47 +0700

    table pdf

--- e4/application/views/report/report_menusa.php
+++ e4/application/views/report/report_menusa.php
@@ -2,14 +2,33 @@
 .ganjil {
 	background-color: #999999
 }
+.t-print{ 
+	border: 1px solid lightblue;
+	border-spacing: 0;
+}
+.t-print th{
+	font-weight: bold;
+	padding-bottom: 10px;
+	padding-top: 10px;
+	font-size: 14px;
+	background-color: lightblue;
+	line-height: 25px;
+}
+.t-print td{
+	line-height: 25px;
+	padding: 3px 10px;
+	font-size: 13px;
+	border: 1px solid lightblue;
+	
+}
 </style>
 <div id="header" align="center">
 	<h1><?php echo $pageTitle;?></h1>
 </div>
-<table width="100%">
+<table width="100%" class="t-print">
 	<tr>
-		<th colspan='2'>TINDAK LANJUT HASIL PENGAWASAN</th>
-		<th colspan='2'>KERUGIAN KEUANGAN NEGARA</th>
+		<th colspan='2' align="center" style="font-weight: bold;">TINDAK LANJUT HASIL PENGAWASAN</th>
+		<th colspan='2' align="center">KERUGIAN KEUANGAN NEGARA</th>
 	</tr>
 	<tr>
 		<td>TOTAL TEMUAN SEJAK TAHUN 2013</td>
