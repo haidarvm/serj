b1d1348d8e4d922cf7639f5bf4ac9a352f077084
alfon_ <alfon@fabelio.com>
2016-08-02 11:31:27 +0700

    tabel pdf

--- e4/application/views/report/report_menusa.php
+++ e4/application/views/report/report_menusa.php
@@ -32,38 +32,38 @@
 	</tr>
 	<tr>
 		<td>TOTAL TEMUAN SEJAK TAHUN 2013</td>
-		<td>: 50</td>
+		<td>50</td>
 		<td>TOTAL TEMUAN SEJAK TAHUN 2013</td>
-		<td>: 50</td>
+		<td>50</td>
 	</tr>
 	<tr>
 		<td>TOTAL REKOMENDASI SEJAK TAHUN 2013</td>
-		<td>: 70</td>
+		<td>70</td>
 		<td>TOTAL REKOMENDASI SEJAK TAHUN 2013</td>
-		<td>: 70</td>
+		<td>70</td>
 	</tr>
 	<tr>
 		<td>SELESAI DITINDAKLANJUTI</td>
-		<td>: 50</td>
+		<td>50</td>
 		<td>SELESAI DITINDAKLANJUTI</td>
-		<td>: 50</td>
+		<td>50</td>
 	</tr>
 	<tr>
 		<td>BELUM SESUAI REKOMENDASI</td>
-		<td>: 15</td>
+		<td>15</td>
 		<td>BELUM SESUAI REKOMENDASI</td>
-		<td>: 15</td>
+		<td>15</td>
 	</tr>
 	<tr>
 		<td>BELUM DITINDAKLANJUTI</td>
-		<td>: 30</td>
+		<td>30</td>
 		<td>BELUM DITINDAKLANJUTI</td>
-		<td>: 30</td>
+		<td>30</td>
 	</tr>
 	<tr>
 		<td>TIDAK DAPAT DITINDAKLANJUTI</td>
-		<td>: 2</td>
+		<td>2</td>
 		<td>TIDAK DAPAT DITINDAKLANJUTI</td>
-		<td>: 2</td>
+		<td>2</td>
 	</tr>
 </table>
