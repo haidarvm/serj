aafa8aaa81acc368ff0c832f079e4a64ff340f68
haidar <haidarvm@gmail.com>
2016-08-08 00:17:37 +0700

    print laporan dashboard

--- e4/application/views/tlhp/menusa_print.php
+++ e4/application/views/tlhp/menusa_print.php
@@ -112,10 +112,75 @@
 	</div>
 	
 </div>
-<script src="<?php echo base_url()?>assets/js/jquery-2.1.3.min.js"></script>
-<script src="<?php echo base_url()?>assets/js/jquery-2.1.3.min.js"></script>
+<script src="<?php echo base_url()?>assets/js/jquery-3.1.0.min.js"></script>
 <script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url()?>assets/js/canvasjs.min.js"></script>
 <script>
+
+
+$(function(){
+	var tindak_lanjut = new CanvasJS.Chart("tindak_lanjut",
+			{
+//				title:{
+//					text: "TLHP"
+//				},
+		                animationEnabled: true,
+				legend:{
+					verticalAlign: "bottom",
+					horizontalAlign: "center"
+				},
+				data: [
+				{        
+					indexLabelFontSize: 20,
+					indexLabelFontFamily: "Monospace",       
+					indexLabelFontColor: "darkgrey", 
+					indexLabelLineColor: "darkgrey",        
+					indexLabelPlacement: "outside",
+					type: "pie",       
+					showInLegend: true,
+					toolTipContent: "{y} - <strong>#percent%</strong>",
+					dataPoints: [
+						{  y: 38, legendText:"belum tl", indexLabel: "BELUM TL : 38" },
+						{  y: 45, legendText:"selesai tl", indexLabel: "SELESAI TL : 45" },
+						{  y: 17, legendText:"belum sesuai",exploded: true, indexLabel: "BELUM SESUAI : 17" },
+						{  y: 1, legendText:"tidak dapat tl" , indexLabel: "TIDAK DAPAT TL : 1"},
+					]
+				}
+				]
+			});
+	tindak_lanjut.render();
+});
+
+$(function(){
+	var kerugian = new CanvasJS.Chart("kerugian",
+			{
+//				title:{
+//					text: "KERUGIAN KEUANGAN NEGARA"
+//				},
+		                animationEnabled: true,
+				legend:{
+					verticalAlign: "bottom",
+					horizontalAlign: "center"
+				},
+				data: [
+				{        
+					indexLabelFontSize: 20,
+					indexLabelFontFamily: "Monospace",       
+					indexLabelFontColor: "darkgrey", 
+					indexLabelLineColor: "darkgrey",        
+					indexLabelPlacement: "outside",
+					type: "pie",       
+					showInLegend: true,
+					toolTipContent: "{y} - <strong>#percent%</strong>",
+					dataPoints: [
+						{  y: 62, legendText:"telah disetor", indexLabel: "TELAH DISETOR : 62%" },
+						{  y: 38, legendText:"belum disetor", indexLabel: "BELUM DISETOR : 38%" }
+					]
+				}
+				]
+			});
+	kerugian.render();
+});
 
 //	console.debug(site_url+ "assets/js/paper-dashboard");
     /*requirejs.config({
