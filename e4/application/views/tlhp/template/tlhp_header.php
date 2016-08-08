9053fd358862f4e68a7c3e2679b4696a860ee6e1
haidar <haidarvm@gmail.com>
2016-08-08 21:43:37 +0700

    remove current app

--- e4/application/views/tlhp/template/tlhp_header.php
+++ e4/application/views/tlhp/template/tlhp_header.php
@@ -103,7 +103,7 @@ var site_url = "<?=base_url()?>";
 					<div class="collapse navbar-collapse .col-md-3">
 						<ul class="nav navbar-nav navbar-right">
 							<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"> <img src="<?=base_url();?>assets/img/faces/face-2.jpg" alt="..." class="img-circle profile-pic">
-									<p>Welcome <?=get_current_app()?>, <?=$_SESSION['full_name']?><br /> <?=$_SESSION['user_level']?></p> <b class="caret"></b>
+									<p>Welcome, <?=$_SESSION['full_name']?><br /> <?=$_SESSION['user_level']?></p> <b class="caret"></b>
 							</a>
 								<ul class="dropdown-menu">
 									<li><a href="<?=site_url()?>tlhp/manus/profile">Edit profile</a></li>
