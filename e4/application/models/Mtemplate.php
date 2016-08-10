418fa8273a44951e897d15072b5d4d1091613a40
haidar <haidarvm@gmail.com>
2016-08-10 17:55:00 +0700

    template laporan update

--- e4/application/models/Mtemplate.php
+++ e4/application/models/Mtemplate.php
@@ -5,22 +5,21 @@
  *
  */
 class Mtemplate extends CI_Model {
-	
+
 	function __construct() {
 		parent::__construct();
 	}
-	
-	
+
 	public function insertTemplateLaporan($data) {
 		unset($data['files']);
 		unset($data['file']);
 		$data['tanggal_laporan'] = sqlDateFormat($data['tanggal_laporan']);
 		$query = $this->db->insert('template_laporan', $data);
 		return $this->db->insert_id();
-		// 		echo $this->db->last_query();exit;
+		// echo $this->db->last_query();exit;
 	}
-	
-	public function getAllTemplate($cond = NULL, $order_by = NULL){
+
+	public function getAllTemplate($cond = NULL, $order_by = NULL) {
 		$cond = ! empty($cond) ? " WHERE 1=1  " . $cond : null;
 		$sql = "SELECT *, tl.template_laporan_id as template_laporan_id, tl.create_date as create_date
 				FROM {PRE}template_laporan tl
@@ -28,29 +27,34 @@ class Mtemplate extends CI_Model {
     			LEFT JOIN {PRE}upload_template_laporan utl ON tlm.upload_template_id = utl.upload_template_id
     			" . $cond . " " . $order_by;
 		$query = $this->db->query($sql);
-		// 		echo $this->db->last_query();exit;
+		// echo $this->db->last_query();exit;
 		return $query;
 	}
-	
+
 	public function getTemplate($id) {
 		$query = $this->db->get_where('template_laporan', array('template_laporan_id' => $id));
-		// 		echo $this->db->last_query();exit;
+		// echo $this->db->last_query();exit;
 		return checkRow($query);
 	}
-	
-	
-	public function updateTemplateLaporan($data,$template_laporan_id) {
+
+	public function updateTemplateLaporan($data, $template_laporan_id) {
 		unset($data['file']);
 		unset($data['files']);
 		return $this->db->update('template_laporan', $data, array('template_laporan_id' => $template_laporan_id));
 	}
-	
 
 	public function insertMedia($data) {
 		$this->db->insert_batch("upload_template_laporan", $data);
-		return $this->db->insert_id();
+		$upload_template_id = $this->db->insert_id();
+		return $this->renameFile($upload_template_id);
+// 		return true;?
 	}
-	
+
+	public function renameFile($upload_tempalate_id) {
+		$data['file_name'] = $upload_tempalate_id;
+		return $this->db->update('upload_template_laporan', $data, array('upload_template_id' => $upload_tempalate_id));
+	}
+
 	public function insertTemplateLaporanMedia($data) {
 		$this->db->insert_batch("template_laporan_media", $data);
 		return $this->db->insert_id();
