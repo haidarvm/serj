ccea9708ec159376467e6986a80fb0f8f19171ff
Atang Sutisna <atang.sutisna.87@gmail.com>
2016-08-18 02:19:57 +0700

    rename redirect controller

--- e4/assets/js/pilihlhp.js
+++ e4/assets/js/pilihlhp.js
@@ -42,7 +42,7 @@ define(["jquery", "knockout", "notify"], function($, ko){
 			if (selectedLhp == undefined) {
 				alert("Lhp tidak boleh kosong");
 			} else {
-				window.location = site_url+"tlhp/lhp/edit?lhp_id="+selectedLhp.lhp_id;
+				window.location = site_url+"tlhp/kkt/edit?lhp_id="+selectedLhp.lhp_id;
 			}
 		}
 		
